d6faebc2001a45e73958cd41672f2ca735dedb4b
Martin Dvoracek <Dworza@gmail.com>
2020-03-02 03:36:02 +0100

    formatting of ProductNotFoundException.java

--- application/src/main/java/com/dvoracek/exercise/application/product/ProductNotFoundException.java
+++ application/src/main/java/com/dvoracek/exercise/application/product/ProductNotFoundException.java
@@ -2,6 +2,6 @@ package com.dvoracek.exercise.application.product;
 
 public class ProductNotFoundException extends RuntimeException {
     public ProductNotFoundException(Long id) {
-        super("Product with id:" + id + " wasn't found.");
+        super("Product with id: " + id + " wasn't found.");
     }
 }
